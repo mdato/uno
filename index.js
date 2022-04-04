28abbdb2bbadf960f5c973125d60684b0fe2ea03
Marcelo Alicia <85354798+mdato@users.noreply.github.com>
2022-04-04 18:05:28 +0000

    new product was added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //console.log('hello capo !!!') // hello capo !!!
 import {User} from "./User.js"
+import {Product} from "./Product.js"
 const pappo = new User('Pappo', 'Blues')
 console.log(pappo) // User { name: 'Pappo', lastName: 'Blues' }
+console.log(new Product('compact', 850)) //Product { name: 'compact', price: 850 }
